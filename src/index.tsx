45c9c1a49eba71e50847df41854662b29beb2ed2
GrafSoul <grafsoul@gmail.com>
2021-12-23 17:55:25 +0500

    feat: Added DragEvent type.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 
 import GuestList from "./state/GuestList";
 import UserSearch from "./state/UserSearch";
+import EventComponent from "./events/EventComponent";
 
 const App: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const App: React.FC = () => {
 
       <GuestList />
       <UserSearch />
+      <EventComponent />
     </div>
   );
 };
